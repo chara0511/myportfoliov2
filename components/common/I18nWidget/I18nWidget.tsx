6e8292128b4826a303e56e829e029c3317143427
Chara- <escor05_11@hotmail.com>
2020-12-25 17:10:33 -0500

    more styles in i18nwidget

--- components/common/I18nWidget/I18nWidget.tsx
+++ components/common/I18nWidget/I18nWidget.tsx
@@ -6,47 +6,49 @@ import styled from 'styled-components'
 import { breakpoints, mixins } from 'styles'
 
 const StyledContent = styled.nav`
-  border: 1px solid red;
   position: absolute;
-  bottom: 5rem;
+  bottom: 1rem;
   right: 0;
+`
 
-  & button {
-    color: ${({ theme }) => theme.colors.text};
-    border: 1px solid blue;
-    font-family: ${({ theme }) => theme.fontMono};
-    padding: 0.5rem;
-    border-radius: 9999px;
+const StyledDropUpMenuBtn = styled.button`
+  color: ${({ theme }) => theme.colors.text};
+  border: 2px solid ${({ theme }) => theme.colors.secondaryBg};
+  font-family: ${({ theme }) => theme.fontMono};
+  padding: 0.5rem;
+  border-radius: 9999px;
+  width: 100%;
 
-    ${mixins.flexBetween};
+  ${mixins.flexBetween};
 
-    & img {
-      width: 28px;
-      height: 28px;
-    }
+  :focus {
+    outline: none;
+  }
 
-    & .visible {
-      display: none;
-    }
+  & img {
+    width: 28px;
+    height: 28px;
+  }
+
+  & .visible {
+    display: none;
   }
 
   @media (min-width: ${breakpoints.md}) {
-    max-width: 150px;
-    width: 100%;
+    width: 112px;
 
-    & button {
-      width: 100%;
-      border-radius: ${({ theme }) => theme.borderRadius.default};
-
-      & img {
-        width: 24px;
-        height: 24px;
-      }
-
-      & .visible {
-        display: block;
-      }
+    & .visible {
+      display: block;
     }
+  }
+`
+
+const StyledWidgetBtn = styled(StyledDropUpMenuBtn)`
+  border: 2px solid ${({ theme }) => theme.colors.secondaryBg};
+  margin-top: 0.5rem;
+
+  @media (min-width: ${breakpoints.md}) {
+    width: 150px;
   }
 `
 
@@ -87,7 +89,39 @@ const I18nWidget: FC = () => {
   return (
     <StyledContent>
       <div>
-        <button aria-label="Language selector" type="button" onClick={() => setDisplay(!display)}>
+        {options?.length && display ? (
+          <>
+            {/* <div>
+              <button type="button" onClick={() => setDisplay(false)} aria-label="Close panel">
+                x
+              </button>
+            </div> */}
+            <ul>
+              {options.map((locale) => (
+                <li key={locale}>
+                  <Link href={currentPath} locale={locale}>
+                    <a>
+                      <StyledDropUpMenuBtn type="button" onClick={() => setDisplay(false)}>
+                        <img
+                          src={`/${LOCALES_MAP[locale].img.filename}`}
+                          alt={LOCALES_MAP[locale].img.alt}
+                        />
+                        <span className="visible">{LOCALES_MAP[locale].name}</span>
+                      </StyledDropUpMenuBtn>
+                    </a>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </>
+        ) : null}
+      </div>
+      <div>
+        <StyledWidgetBtn
+          aria-label="Language selector"
+          type="button"
+          onClick={() => setDisplay(!display)}
+        >
           <img
             src={`/${LOCALES_MAP[currentLocale].img.filename}`}
             alt={LOCALES_MAP[currentLocale].img.alt}
@@ -98,31 +132,7 @@ const I18nWidget: FC = () => {
               <ChevronUpIcon className="visible" />
             </span>
           )}
-        </button>
-      </div>
-      <div className="absolute top-0 right-0">
-        {options?.length && display ? (
-          <div>
-            <div>
-              <button type="button" onClick={() => setDisplay(false)} aria-label="Close panel">
-                x
-              </button>
-            </div>
-            <ul>
-              {options.map((locale) => (
-                <li key={locale}>
-                  <Link href={currentPath} locale={locale}>
-                    <a>
-                      <button type="button" onClick={() => setDisplay(false)}>
-                        {LOCALES_MAP[locale].name}
-                      </button>
-                    </a>
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </div>
-        ) : null}
+        </StyledWidgetBtn>
       </div>
     </StyledContent>
   )
