b416579275475f5b29cb4b7445d0b5f4afbd1295
Chara- <escor05_11@hotmail.com>
2021-03-12 13:46:57 -0500

    caret dependencies removed

--- components/common/Head/Head.tsx
+++ components/common/Head/Head.tsx
@@ -10,7 +10,11 @@ const Head: FC = () => {
       <DefaultSeo {...config} />
       <NextHead>
         <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no" />
-        <link rel="shortcut icon" href="/favicon.ico" />
+        {/* <link rel="icon" href="/favicon.ico" /> */}
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+
         <link rel="manifest" href="/site.webmanifest" key="site-manifest" />
         {/* <link rel="manifest" href="/manifest.json" key="site-manifest" /> */}
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
